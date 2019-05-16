8e0b4f50409265b5f0a708ad23f6eae5014ee000
Vesta080283 <43104215+Vesta080283@users.noreply.github.com>
2019-05-16 19:33:12 +0700

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const mongo = require('mongodb').MongoClient;
+const routes = require('./routes.js');
+const auth = require('./auth.js');
+const GitHubStrategy = require('passport-github').Strategy;
+const passport = require('passport');
+const app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'pug')
+
+mongo.connect(process.env.DATABASE, (err, db) => {
+    if (err) {
+        console.log('Database error: ' + err);
+    } else {
+        console.log('Successful database connection');
+
+        auth(app, db);
+
+        routes(app, db);
+
+        passport.use(new GitHubStrategy({
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: 'https://glitch.com/edit/#!/cake-beet/auth/github/callback'
+        },
+            function (accessToken, refreshToken, profile, cb) {
+                console.log(profile);
+                //Database logic here with callback containing our user object
+                db.collection('socialusers').findAndModify(
+                    { id: profile.id },
+                    {},
+                    {
+                        $setOnInsert: {
+                            id: profile.id,
+                            name: profile.displayName || 'John Doe',
+                            photo: profile.photos[0].value || '',
+                            email: profile.emails[0].value || 'No public email',
+                            created_on: new Date(),
+                            provider: profile.provider || ''
+                        }, $set: {
+                            last_login: new Date()
+                        }, $inc: {
+                            login_count: 1
+                        }
+                    },
+                    { upsert: true, new: true },
+                    (err, doc) => {
+                        return cb(null, doc.value);
+                    }
+                );
+            }
+        ));
+
+
+        app.listen(process.env.PORT || 3000, () => {
+            console.log("Listening on port " + process.env.PORT);
+        });
+    }
+});
